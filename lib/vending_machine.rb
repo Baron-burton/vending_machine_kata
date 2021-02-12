182b34222d8c82edb91cf2f6aae034f2ef5596ca
Brandon Burton <brandon.anthony.burton@gmail.com>
2021-02-12 21:29:22 +0000

    A VendingMachine can take an initial load of coins

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,7 +1,9 @@
 require_relative 'inventory'
+require_relative 'coin_drawer'
 
 class VendingMachine
-  def initialize(products = nil)
+  def initialize(products = nil, coins = nil)
     @inventory = Inventory.new(products)
+    @coin_drawer = CoinDrawer.new(coins)
   end
 end
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe VendingMachine do
 
     VendingMachine.new(products)
   end
+
+  it 'can take an initial load of coins' do
+    coins = [ 1, 2, 5, 10, 20, 50, 100, 200 ]
+
+    expect(CoinDrawer).to receive(:new).with(coins)
+
+    VendingMachine.new(nil, coins)
+  end
 end
